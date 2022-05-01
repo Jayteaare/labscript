242187f6be785447911d26edbf3505e791d9c455
Jayteaare <94884280+Jayteaare@users.noreply.github.com>
2022-04-30 22:40:44 -0400

    Create lab.sh

---
+++ lab.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Hello there! star{warz}" > ~/Desktop/kenobi.txt
+echo "General Kenobi.. star{warz} order{66}" > ~/grievous.txt
+echo "Do, or do not. There is no try. order{66}" > /tmp/yoda.txt
+
+
+mkdir ~/tools/yarGen/malwarez 
+echo "VGhpcyBpcyBub3QgdGhlIGRyb2lkIHlvdSBhcmUgbG9va2luZyBmb3Iu O66. DV. LS. #%%%^storm##tropper" > ~/tools/yarGen/malwarez/deathstar.txt
